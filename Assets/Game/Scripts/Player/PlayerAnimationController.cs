03615ed0825ac7ea03f7f35c7738286e6e4ecd0f
Gazi Akviran <gaziakvrn@gmail.com>
2025-02-22 01:18:21 +0300

    Level end camera rotation system implemented

--- Assets/Game/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Game/Scripts/Player/PlayerAnimationController.cs
@@ -9,7 +9,8 @@ public class PlayerAnimationController : MonoBehaviour
     static readonly int IsRunning = Animator.StringToHash("isRunning");
     static readonly int IsDancing = Animator.StringToHash("isDancing");
     static readonly int IsIdle = Animator.StringToHash("isIdle");
-    
+
+    [SerializeField, BoxGroup("Camera Animation")] private float rotateDuration = 15;
     [SerializeField, Foldout("References")] private Animator animator;
     [SerializeField, Foldout("References")] private Transform cameraRotateTransform;
 
@@ -33,7 +34,7 @@ public class PlayerAnimationController : MonoBehaviour
 
     public void RotateCameraTransform()
     {
-        cameraRotateTransform.DOLocalRotate(new Vector3(0, 360, 0), 5f, RotateMode.FastBeyond360)
+        cameraRotateTransform.DOLocalRotate(new Vector3(0, 360, 0), rotateDuration, RotateMode.FastBeyond360)
             .SetEase(Ease.Linear)
             .SetLoops(-1, LoopType.Restart);
     }
